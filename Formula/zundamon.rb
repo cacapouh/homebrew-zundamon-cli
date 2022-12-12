1d1cb4a171902ddb9437602077e8154fe413b18f
cacapouh <>
2022-12-12 10:30:37 +0000

    Init

---
+++ Formula/zundamon.rb
@@ -0,0 +1,13 @@
+class Zundamon < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/cacapouh/zundamon-cli/releases/download/0.0.1/package.tar.gz"
+  sha256 "9e07905d243c72cc609917dab6bc706fcf3c0905881243922c1e401feb132164"
+  license ""
+
+  def install
+    bin.install "zundamon"
+    prefix.install "libcore.dylib", "libonnxruntime.1.10.0.dylib", "open_jtalk_dic_utf_8-1.11"
+  end
+end
+
